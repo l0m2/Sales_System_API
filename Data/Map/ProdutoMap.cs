a9cae9d376029ae8fa19899fd37b7a8822ee3f11
Leopoldo Jacob Matsinhe <121976617+l0m2@users.noreply.github.com>
2023-09-30 23:18:58 +0200

    ProdutoMap

---
+++ Data/Map/ProdutoMap.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Sales_System_API.Model;
+
+namespace Sales_System_API.Data.Map
+{
+    public class ProdutoMap 
+    {
+     
+    }
+}
